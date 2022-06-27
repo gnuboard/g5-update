6a35a4e8df4a391474916b3ccdf6066d53176b49
kjh <kjhied@gmail.com>
2022-06-27 09:10:50 +0000

    [chore] 최신버전 v5.5.8.1.1 적용

--- adm/_common.php
+++ adm/_common.php
@@ -1,7 +1,7 @@
 <?php
 define('G5_IS_ADMIN', true);
-include_once('../common.php');
-include_once(G5_ADMIN_PATH . '/admin.lib.php');
+require_once '../common.php';
+require_once G5_ADMIN_PATH . '/admin.lib.php';
 
 if (isset($token)) {
     $token = @htmlspecialchars(strip_tags($token), ENT_QUOTES);
