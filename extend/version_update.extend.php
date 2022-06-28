b4db4191d6430e3292dc17a1ae1740e58c98f4dd
kjh <kjhied@gmail.com>
2022-06-28 06:19:07 +0000

    [style] 버전 업데이트 extend파일 수정
    - phpcs 적용

---
+++ extend/version_update.extend.php
@@ -0,0 +1,20 @@
+<?php
+// 개별 페이지 접근 불가
+if (!defined('_GNUBOARD_')) {
+    exit;
+}
+
+add_replace('admin_menu', 'add_admin_menu_update', 1, 1);
+
+function add_admin_menu_update($menu)
+{
+    $menu_num = '100600';
+
+    if (!isset($menu['menu100'])) {
+        return $menu;
+    }
+
+    array_push($menu['menu100'], array($menu_num, '버전 업데이트', G5_ADMIN_URL.'/update/', 'update'));
+
+    return $menu;
+}
