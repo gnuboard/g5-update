63887a92869c845430c47c11d7346dec032b6959
kir rio <mario64aq@gmail.com>
2022-11-17 10:38:58 +0900

    구독 서비스 목록, 상세보기 추가

--- bbs/subscription/subscription_service.php
+++ bbs/subscription/subscription_service.php
@@ -3,6 +3,104 @@
 require_once(dirname(__FILE__, 3) . '/common.php');
 //require_once(dirname(__FILE__, 1) . '/kcp-batch/G5Mysqli.php');
 
+
+/**
+ * 서비스 선택 후 상세 보기
+ * @return array
+ * @var int $serviceId
+ * @return array 결과 없으면 빈배열 리턴
+ */
+function showServiceDetail($serviceId)
+{
+    $selectByIdSql = 'select
+       service.service_id,
+       bo_table,
+       service_name,
+       service_summary,
+       service_explan,
+       service_mobile_explan,
+       service_image,
+       service_url,
+       service_hook,
+       service_expiration,
+       service_expiration_unit,
+       service_order,
+       service_use,
+       recurring_count,
+       recurring_unit,
+       price,
+       price_table.apply_date as price_apply_date
+    from ' . G5_TABLE_PREFIX . 'batch_service as service
+    left join ' . G5_TABLE_PREFIX . 'batch_service_price as price_table
+        on service.service_id = price_table.service_id
+    where service.service_id = ' . sql_real_escape_string($serviceId);
+
+    $result = sql_query($selectByIdSql);
+    $responseItem = array();
+
+        while ($row = sql_fetch_array($result)) {
+            $responseItem[] = $row;
+        }
+
+
+    return $responseItem;
+}
+
+/**
+ * 모든 구독 서비스 목록
+ * @param int $pageNo 몇 쪽 0쪽 부터 시작.
+ * @param int $pagePerCount 몇개씩 보여줄지
+ * @return array 결과 없으면 빈배열 리턴
+ */
+function showServiceList($pageNo, $pagePerCount)
+{
+    $startPage = $pageNo * $pagePerCount;
+    $lastPage = $startPage + $pagePerCount;
+
+    ++$startPage; //예) 1p 일 때   11, 20
+    if ($pageNo == 0) {
+        $startPage = 0;
+    }
+
+    if ($pageNo < 0 || $lastPage < 0) {
+        $startPage = 0;
+        $lastPage = 0;
+    }
+
+    $selectAllServiceSql = 'select
+    service.service_id,
+    bo_table,
+    service_name,
+    service_summary,
+    service_explan,
+    service_mobile_explan,
+    service_image,
+    service_url,
+    service_hook,
+    service_expiration,
+    service_expiration_unit,
+    service_order,
+    service_use,
+    recurring_count,
+    recurring_unit,
+    price,
+    price_table.apply_date as price_apply_date
+    from ' . G5_TABLE_PREFIX . 'batch_service as service
+    left join ' . G5_TABLE_PREFIX . 'batch_service_price as price_table
+        on service.service_id = price_table.service_id order by service_order asc limit '
+        . sql_real_escape_string($startPage) . ', ' . sql_real_escape_string($lastPage);
+
+    $result = sql_query($selectAllServiceSql);
+    $responseItem = array();
+    if ($result) {
+        while ($row = sql_fetch_array($result)) {
+            $responseItem[] = $row;
+        }
+    }
+
+    return $responseItem;
+}
+
 /**
  * @param $service_id
  * @return bool
