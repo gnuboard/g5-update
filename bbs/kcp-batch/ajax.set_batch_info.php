0d6298682dc8294eb983195a5fd322fde7854857
kir rio <mario64aq@gmail.com>
2022-11-23 17:48:42 +0900

    pc 결제창 변수 이름규칙 수정

--- bbs/kcp-batch/ajax.set_batch_info.php
+++ bbs/kcp-batch/ajax.set_batch_info.php
@@ -7,29 +7,35 @@ $input_data = json_decode(file_get_contents('php://input'), true);
 /**
  * @param string $w working mode 작업모드.
  */
-$w = isset($input_data['w']) ? $input_data['w'] : '';
+$work = isset($input_data['w']) ? $input_data['w'] : '';
 
 //필수 파라미터
-if (empty($input_data) || $is_guest === true || empty($w)) {
+if (empty($input_data) || $is_guest === true || empty($work)) {
     responseJson('잘못된 요청입니다.', 400);
 }
 
-if ($w === 'getBatchInfo') {
-    $serviceId = isset($input_data['serviceId']) ? $input_data['serviceId'] : '';
-    $orderId = isset($input_data['orderId']) ? $input_data['orderId'] : '';
+if ($work === 'get_info') {
+    $service_id = isset($input_data['service_id']) ? $input_data['service_id'] : '';
+    $order_id = isset($input_data['order_id']) ? $input_data['order_id'] : '';
 
     //유효성 검사
-    if (empty($serviceId) || empty($orderId)) {
+    if (empty($service_id) || empty($order_id)) {
         responseJson('잘못된 요청입니다.', 400);
         exit;
     }
 
-    sendBatchInfo($orderId, $serviceId);
+    send_batch_info($order_id, $service_id);
 }
 
-function sendBatchInfo($orderId, $serviceId)
+
+/**
+ * @param $order_id
+ * @param $service_id
+ * @return void
+ */
+function send_batch_info($order_id, $service_id)
 {
-    $result = getBatchKeyInfoKcp($orderId, $serviceId);
+    $result = get_batchkey_info_kcp($order_id, $service_id);
     if ($result === false) {
         responseJson('결제정보를 가져오는데 실패했습니다.', 400);
     }
@@ -38,29 +44,30 @@ function sendBatchInfo($orderId, $serviceId)
 
 /**
  * PG 사에 배치키 발급 요청정보를 설정 PC.
- * @param $serviceId
+ * @param $order_id
+ * @param $service_id
  * @return array | false
  */
-function getBatchKeyInfoKcp($orderId, $serviceId)
+function get_batchkey_info_kcp($order_id, $service_id)
 {
-    $info = showServiceDetail($serviceId);
+    $info = showServiceDetail($service_id);
     if (is_array($info) && count($info) !== 1) {
         return false;
     }
 
-    $serviceRecurringCount = $info[0]['recurring_count'];
-    $serviceUnit = $info[0]['recurring_unit'];
-    if($serviceUnit === 'w'){
-        $serviceRecurringCount *= 7;
-        $serviceUnit = 'd';
-    }
-    $good_expr =  '2:' . $serviceRecurringCount . $serviceUnit;
+    $kcp_batch = new KcpBatch();
 
-    $kcpBatch = new KcpBatch();
+    $recurring_count = $info[0]['recurring_count'];
+    $recurring_unit = $info[0]['recurring_unit'];
+    if($recurring_unit === 'w'){
+        $recurring_count *= 7;
+        $recurring_unit = 'd';
+    }
+    $good_expr =  '2:' . $recurring_count . $recurring_unit;
     $sendInfo = array();
-    $sendInfo['site_cd'] = $kcpBatch->getSiteCd();
-    $sendInfo['ordr_idxx'] = $orderId;
-    $sendInfo['kcpgroup_id'] = $kcpBatch->getKcpGroupId();
+    $sendInfo['site_cd'] = $kcp_batch->getSiteCd();
+    $sendInfo['ordr_idxx'] = $order_id;
+    $sendInfo['kcpgroup_id'] = $kcp_batch->getKcpGroupId();
     $sendInfo['pay_method'] = 'AUTH:CARD';
     $sendInfo['card_cert_type'] = 'BATCH';
     $sendInfo['batch_soc'] = 'Y';
