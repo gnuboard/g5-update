962f4d17ffd34d0074ef459135f2dba26c6ab965
kir rio <mario64aq@gmail.com>
2022-12-05 17:36:50 +0900

    크론 호출 파일 추가

---
+++ bbs/subscription/ajax.batch_service.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once(dirname(__FILE__) . '../../../common.php');
+
+$url = G5_BBS_URL . '/subscription/batch_service.php';
+curl_request_async($url, '' );
+
+/**
+ * 일회성 요청
+ * @param $url
+ * @param $params
+ * @return void
+ */
+function curl_request_async($url, $params)
+{
+    //$req_data       = json_encode($data);
+    $headerData    = array("Content-Type: application/json", "charset=utf-8");
+
+    // API REQ
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headerData);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+
+    //curl_setopt($ch, CURLOPT_POSTFIELDS, $req_data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // API RES
+    curl_exec($ch);
+    $curlError = curl_error($ch);
+    $curlInfo = curl_getinfo($ch);
+    if(define(CURLINFO_HTTP_CODE)){
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+    curl_close($ch);
+
+    $http_code = isset($http_code) ? $http_code : 200;
+    header('', true, $http_code);
+    echo json_encode(array('message' => '결제 작업이 실행되었습니다'));
+
+}
