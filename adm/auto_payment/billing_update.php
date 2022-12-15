15e819a138c969fbf9e9b9913473d64314e975b9
kjh <kjhied@gmail.com>
2022-12-15 17:50:40 +0900

    [fix] 결제종료일 입력 시, price 값 갱신
    - 결제 종료일이 있으면, infomation 테이블의 price값으로 결제되므로 0이 있으면 안됨.

--- adm/auto_payment/billing_update.php
+++ adm/auto_payment/billing_update.php
@@ -6,13 +6,23 @@ auth_check_menu($auth, $sub_menu, 'w');
 check_admin_token();
 
 $information_model  = new BillingInformationModel();
+$price_model        = new BillingServicePriceModel();
 
-$od_id  = isset($_REQUEST['od_id']) ? safe_replace_regex($_REQUEST['od_id'], 'od_id') : '';
-$data   = array(
+$od_id      = isset($_REQUEST['od_id']) ? safe_replace_regex($_REQUEST['od_id'], 'od_id') : '';
+$end_date   = !empty($_POST['end_date']) ? $_POST['end_date'] : null;
+$data       = array(
     'status'            => isset($_POST['status']) ? preg_replace('/[^0-9]/', '', $_POST['status']) : 0,
     'next_payment_date' => isset($_POST['next_payment_date']) ? $_POST['next_payment_date'] : null,
-    'end_date'          => isset($_POST['end_date']) ? $_POST['end_date'] : null
+    'end_date'          => $end_date
 );
+
+// 결제종료일이 입력여부에 따라 price를 업데이트해준다.
+$billing_info = $information_model->selectOneByOrderId($od_id);
+if ($end_date != null) {
+    $data['price'] = $price_model->selectCurrentPrice($billing_info['service_id']);
+} else {
+    $data['price'] = 0;
+}
 
 $information_model->update($od_id, $data);
 
