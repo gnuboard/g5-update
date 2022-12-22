723eba177821d34ca16e9a6f22ab39526572e82c
kir rio <mario64aq@gmail.com>
2022-12-22 18:47:26 +0900

    ajax.mypage.php 수정
    페이징 추가
    구독 취소시 주문번호 파라미터 체크

--- bbs/subscription/ajax.mypage.php
+++ bbs/subscription/ajax.mypage.php
@@ -8,29 +8,38 @@ header('Content-type: application/json; charset=utf-8');
  *  마이페이지 ajax API , 컨트롤러.
  */
 
-$res_data = json_decode(file_get_contents('php://input'), true);
-$work_mode = isset($res_data['w']) ? $res_data['w'] : '';
-if(empty($work_mode)){
+$input_data = json_decode(file_get_contents('php://input'), true);
+$work_mode = isset($input_data['w']) ? $input_data['w'] : '';
+if (empty($work_mode)) {
     response_json('잘못된 요청입니다.', 400);
 }
 
 if($_SERVER['REQUEST_METHOD'] === 'GET') {
     switch ($work_mode) {
         case  'all' : {
-            $result =  get_myservice(1);//TODO 페이징
-            if($result === false){
+            //그누보드 페이징은 1 부터시작.
+            $page = isset($input_data['page']) ? $input_data['page'] : 1;
+            $rows = isset($input_data['rows']) ? $input_data['rows'] : 10;
+            $offset = ($page - 1) * $rows; // 시작 열을 구함
+            $query_data = array(
+                'status' => 1,
+                'offset' => $offset,
+                'rows' => $rows
+            );
+
+            $result = get_myservice($query_data);
+            if ($result === false) {
                 response_json('잘못된 요청입니다.', 400);
             } else {
-                if (PHP_VERSION_ID >= 50400) {
-                    echo json_encode($res_data, JSON_UNESCAPED_UNICODE);
-                } else {
-                    echo to_han(json_encode($res_data));
-                }
+                response_json($result);
             }
         }
         break;
         case 'id' : {
-            echo '';
+            if(!isset($input_data['od_id']) || empty($input_data['od_id'])){
+                response_json('요청파라미터가 잘못되었습니다', 400);
+            }
+            get_myservice_info($input_data['od_id']);
         }
             break;
 
@@ -38,12 +47,14 @@ if($_SERVER['REQUEST_METHOD'] === 'GET') {
 }
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
     switch ($work_mode) {
         case 'cancel' : {
-            $od_id = $res_data['od_id'];
-            $result = cancel_myservice($od_id);
-            if($result === false){
+            if(!isset($input_data['od_id']) || empty($input_data['od_id'])){
+                response_json('요청파라미터가 잘못되었습니다', 400);
+            }
+
+            $result = cancel_myservice($input_data['od_id']);
+            if ($result === false) {
                 response_json('잘못된 요청입니다.', 400);
             } else {
                 echo json_encode($result);
