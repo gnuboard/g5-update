9508558d954e498cccf2fe22b8e216503b8ee93c
kir rio <mario64aq@gmail.com>
2022-12-23 18:49:00 +0900

    관리자, 사용자 빌링키 발급 api 오류 수정
    ajax.get_billing_key.php
    공통 수정사항
    result_code 가 0000 이 아닐 때
    billing_key_history 테이블 저장배열의 undefined 수정
    그로 인한 json 응답 오류 수정.
    
    개별 수정사항
    관리자
    카드정보 업데이트에 필요한
    기존 id 불러오기 추가

--- adm/auto_payment/ajax.get_billing_key.php
+++ adm/auto_payment/ajax.get_billing_key.php
@@ -5,44 +5,62 @@ include_once './_common.php';
 $billing            = new Billing($billing_conf['bc_pg_code']);
 $information_model  = new BillingInformationModel();
 $key_history_model  = new BillingKeyHistoryModel();
+$billing_history_model = new BillingHistoryModel();
 
 /* ============================================================================== */
 /* =  요청정보                                                                   = */
 /* = -------------------------------------------------------------------------- = */
 $od_id = clean_xss_tags($_POST['ordr_idxx']);
-$mb_id = clean_xss_tags($_POST['mb_id']);
 $card_no = clean_xss_tags($_POST['card_mask_no']);
+
+//구독정보의 카드 업데이트에 회원 id가 필요.
+$billing_info = $information_model->selectOneByOrderId($od_id);
+
+if (empty($od_id) || empty($billing_info) || !isset($billing_info['mb_id'])) {
+    response_json('필수 파라미터가 없습니다.', 400);
+}
+
+$mb_id = $billing_info['mb_id'];
+
 /* ============================================================================== */
 /* =  요청                                                                      = */
 /* = -------------------------------------------------------------------------- = */
-$res_data = $billing->pg->requestIssueBillKey($_POST);
-$res_data = $billing->convertPgDataToCommonData($res_data);
+$pg_res_data = $billing->pg->requestIssueBillKey($_POST);
+$pg_res_data = $billing->convertPgDataToCommonData($pg_res_data);
 /* ============================================================================== */
 /* =  응답정보                                                                     = */
 /* = -------------------------------------------------------------------------- = */
-// Res JSON DATA Parsing
-if (isset($res_data['http_code'])) {
-    response_json($res_data['result_message'], $res_data['http_code']);
+
+// pg 에러
+if (isset($pg_res_data['http_code'])) {
+    response_json($pg_res_data['result_message'], $pg_res_data['http_code']);
 }
 
-$res_data['pg_code'] = $billing_conf['bc_pg_code'];
-$res_data['card_no'] = $card_no;
-$res_data['od_id'] = $od_id;
-$res_data['mb_id'] = $mb_id;
+$result_code = isset($pg_res_data['result_code']) ? $pg_res_data['result_code'] : '';
+$billing_key = isset($pg_res_data['billing_key']) ? $pg_res_data['billing_key'] : '';
 
-$result_code    = $res_data['result_code'];
-$billing_key    = $res_data['billing_key'];
+$history_data = array(
+    'pg_code' => $billing_conf['bc_pg_code'],
+    'od_id' => $od_id,
+    'mb_id' => $mb_id,
+    'card_no' => $card_no,
+    'card_code' => isset($pg_res_data['card_code']) ? $pg_res_data['card_code'] : '',
+    'card_name' => isset($pg_res_data['card_name']) ? $pg_res_data['card_name'] : '',
+    'result_code' => $result_code,
+    'result_message' => isset($pg_res_data['result_message']) ? $pg_res_data['result_message'] : '',
+    'billing_key' => $billing_key
+);
 
 /* ============================================================================== */
-/* =   결과처리 및 반환                                                          = */
+/* =   결과처리 및 출력                                                          = */
 /* ============================================================================== */
 // 로그 테이블 저장
-$key_history_model->insert($res_data);
+$key_history_model->insert($history_data);
 
 // 결제정보 배치 키 변경
-if ($result_code == "0000") {
+if ($result_code === "0000") {
     $information_model->updateBillingKey($od_id, $billing_key);
-    $res_data['display_billing_key'] = $billing->displayBillKey($billing_key);
+    $pg_res_data['display_billing_key'] = $billing->displayBillKey($billing_key);
 }
 // 나머지 결과 출력
-response_json($res_data);
+response_json($pg_res_data);
